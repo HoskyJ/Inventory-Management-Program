c0034d9423e7497c9c24e6cf800305272e1ac1b0
Tim Devereux <devereux.tj@gmail.com>
2018-05-27 19:44:56 +1000

    Removed print statements from main. Formatting in stock.

--- src/Main/Entry.java
+++ src/Main/Entry.java
@@ -1,30 +1,19 @@
 package Main;
-import Exception.CSVFormatException;
+import Stock.Store;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+
+import Exception.CSVFormatException;
+import GUI.GUI;
 
 public class Entry {
-
+	public static Store store;
 	public static void main(String[] args) throws IOException, CSVFormatException {
-		//test readItems
-		String[][] itemProperties = CSV.ParseCSV.readItems("test_files/item_properties(empty).csv");
-		System.out.println("ITEM PROPERTIES " + Arrays.deepToString(itemProperties));
+		//Create store object
+		store = new Store("Store", 100000);
 		
-		//test readSales
-		String[][] salesList = CSV.ParseCSV.readSales("test_files/sales_log_0.csv");
-		System.out.println("SALES LIST " + Arrays.deepToString(salesList));
-		
-		//test readManifest and writeManifest
-		//WriteCSV.WriteManifest("Refridgerated", "icecream", 1);
-		List<List<String>> manifestList = CSV.ParseCSV.readManifest();
-		System.out.println("MANIFEST " + manifestList);
-		//WriteCSV.WriteManifest("Ordinary", "cheese", 9);
-		manifestList = CSV.ParseCSV.readManifest();
-		System.out.println("MANIFEST " + manifestList);
-		
-		System.out.println("MANIFEST " + manifestList);
+		//Start GUI
+		GUI.initializeWindow();
 	}
-
+	
+	
 }
-
